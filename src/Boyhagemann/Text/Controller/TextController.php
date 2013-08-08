8d81ed353da586c74226d7bb1d4be7761fe19b24
boyhagemann <boy@swis.nl>
2013-08-08 14:26:14 +0200

    start

--- src/Boyhagemann/Text/Controller/TextController.php
+++ src/Boyhagemann/Text/Controller/TextController.php
@@ -2,16 +2,13 @@
 
 namespace Boyhagemann\Text\Controller;
 
-use Boyhagemann\Crud\CrudController;
 use Boyhagemann\Form\FormBuilder;
-use Boyhagemann\Model\ModelBuilder;
-use Boyhagemann\Overview\OverviewBuilder;
 
 class TextController extends \BaseController
 {
-	public function text()
+	public function text($text)
 	{
-
+		return $text;
 	}
 
 	public function textPortlet(FormBuilder $fb)
