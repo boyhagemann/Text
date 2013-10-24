89ed3b1debc79e0f71a3a6bbc412099bf6a223eb
= <=>
2013-10-24 16:42:19 +0200

    update text config

--- src/Boyhagemann/Text/Controller/TextController.php
+++ src/Boyhagemann/Text/Controller/TextController.php
@@ -6,13 +6,24 @@ use Boyhagemann\Form\FormBuilder;
 
 class TextController extends \BaseController
 {
-	public function text($text)
+	public function textarea($text)
 	{
 		return $text;
 	}
 
-	public function textPortlet(FormBuilder $fb)
+	public function textareaConfig(FormBuilder $fb)
 	{
+		$fb->textarea('text')->label('Text')->required();
+	}
+
+	public function heading($text)
+	{
+		return $text;
+	}
+
+	public function headingConfig(FormBuilder $fb)
+	{
+		$fb->select('heading')->label('Heading')->choices(array('h1' => 'Biggest', 'h2' => 'Big', 'h3' => 'Medium'));
 		$fb->textarea('text')->label('Text')->required();
 	}
 }
