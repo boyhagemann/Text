88d95cc9eadcc856cb170542e8fb85d94f4ebdc8
Boy Hagemann <boy@swis.nl>
2013-11-17 16:02:15 +0100

    update editor

--- src/Boyhagemann/Text/FormElement/Wysiwyg.php
+++ src/Boyhagemann/Text/FormElement/Wysiwyg.php
@@ -6,7 +6,7 @@ use Boyhagemann\Form\Element\Hidden;
 
 class Wysiwyg extends Hidden
 {
-    protected $view = 'text::element.test';
+    protected $view = 'text::element.wysiwyg';
 
     protected $attributes = array(
         'class' => 'wysiwyg'
--- src/views/add-text-from-section.blade.php
+++ src/views/add-text-from-section.blade.php
@@ -3,6 +3,7 @@
     <div class="col-lg-12">
         
         <a data-toggle="collapse" href="#editor-{{ $section->name }}"><i class="icon-pencil"></i> Add text</a>  
+        <br>
         <div id="editor-{{ $section->name }}" class="collapse">
             {{ $textForm }}        
         </div>
